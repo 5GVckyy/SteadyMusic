80686aabccaf3a80a61426020e1e8f910422b91c
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2021-12-10 06:30:33 +0700

    [STEADY]

--- Yukki/Plugins/Developer.py
+++ Yukki/Plugins/Developer.py
@@ -13,7 +13,7 @@ from pyrogram import filters
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import Message, ReplyKeyboardMarkup
 
-from Yukki import SUDOERS, userbot
+from Yukki import SUDOERS, app, userbot
 from Yukki.Utilities.tasks import add_task, rm_task
 
 # Eval and Sh module from WBB
@@ -81,7 +81,7 @@ async def iter_edit(message: Message, text: str):
                 return
 
 
-@userbot.on_message(
+@app.on_message(
     filters.user(SUDOERS)
     & ~filters.forwarded
     & ~filters.via_bot
@@ -177,7 +177,7 @@ async def executor(client, message: Message):
     await eor(status, text=final_output, quote=True)
 
 
-@userbot.on_message(
+@app.on_message(
     filters.user(SUDOERS)
     & ~filters.forwarded
     & ~filters.via_bot
@@ -216,7 +216,7 @@ async def shellrunner(_, message: Message):
     if str(output) == "\n":
         output = None
     if output:
-        if len(output) > 4096:
+        if len(f"**INPUT:**\n```{escape(text)}```\n\n**OUTPUT:**\n```{(output)}```") > 4118:
             with open("output.txt", "w+") as file:
                 file.write(output)
             await message.reply_document(
