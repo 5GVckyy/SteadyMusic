c833ea999576bab11653cc2dd4a8c5af807adf91
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2021-12-11 17:54:02 +0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -2,7 +2,7 @@ import asyncio
 from os import path
 import psutil
 
-
+from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import (InlineKeyboardMarkup, InputMediaPhoto, Message,
                             Voice, InlineKeyboardButton)
